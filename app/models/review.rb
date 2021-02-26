c734b5f6bf7e1269e46c7439af8702f5c2956e21
jyliu17 <jyliu17@gmail.com>
2021-02-26 17:35:44 -0500

    validation for review ratings

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  validates :rating, :inclusion => 1..5
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,5 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :rating
+  attributes :id, :comment, :rating, :product_id, :user_id, :user, :product
   has_one :user
   has_one :product
 end
